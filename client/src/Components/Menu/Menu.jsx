3ba383822824184d35cf256aa248cf7b10fc7425
Danil101214 <Danilkuznyak30@gmail.com>
2026-04-18 16:03:44 +0200

    It was changed client

--- client/src/Components/Menu/Menu.jsx
+++ client/src/Components/Menu/Menu.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import { arrayMenu } from '../../Arrays/arrayMenu'
+import logo from '../../Images/logo.svg'
+import './style.css'
 
 const Menu = () => {
+  const firstMenu = arrayMenu.slice(0, 2);
+  const secondMenu = arrayMenu.slice(2, 4)
   return (
     <div className='menu__container'>
       <nav className="menu__navigation">
         <ul className="menu__list">
-          {arrayMenu.map((item) => {
-            <li className="menu__item">
+          {firstMenu.map((item) => {
+            return <li className="menu__item">
+              <Link to={item.link} className="menu__link">{item.title}</Link>
+            </li>
+          })}
+          <li className="menu__item">
+            <img src={logo} alt="logo" />
+          </li>
+          {secondMenu.map((item) => {
+            return <li className="menu__item">
               <Link to={item.link} className="menu__link">{item.title}</Link>
             </li>
           })}
--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import profile from '../../Images/profile.svg'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className="profile__container">
+        <img src={profile} alt="profile" className="profile__image" />
+    </div>
   )
 }
 
